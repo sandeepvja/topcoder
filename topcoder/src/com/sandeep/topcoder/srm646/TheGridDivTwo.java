36825c40c675d14989629b0b90de0aa297c0c204
melsa03 <melsa03@MELSA03-M4700.ca.com>
2015-01-17 14:37:22 +0530

    Fixing the 500-point problem

--- topcoder/src/com/sandeep/topcoder/srm646/TheGridDivTwo.java
+++ topcoder/src/com/sandeep/topcoder/srm646/TheGridDivTwo.java
@@ -1,173 +1,197 @@
 package com.sandeep.topcoder.srm646;
 
-import java.util.*;
-import java.util.regex.*;
-import java.text.*;
-import java.math.*;
-import java.awt.geom.*;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
 
-public class TheGridDivTwo {
+
+public class TheGridDivTwo
+{
 	
-	class Return {
+	class Pair{
 		int x;
 		int y;
 		int k;
-		boolean blocked;
 		
-		Return(int x, int y, int k, boolean blocked) {
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getOuterType().hashCode();
+			result = prime * result + x;
+			result = prime * result + y;
+			return result;
+		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Pair other = (Pair) obj;
+			if (!getOuterType().equals(other.getOuterType()))
+				return false;
+			
+			if (x != other.x)
+				return false;
+			if (y != other.y)
+				return false;
+			return true;
+		}
+		public Pair(int x, int y, int k) {
+			super();
 			this.x = x;
 			this.y = y;
 			this.k = k;
-			this.blocked = blocked;
-		}
-		public void ret(){
-			blocked = true;
 		}
 		public int getX() {
 			return x;
 		}
+		public void setX(int x) {
+			this.x = x;
+		}
 		public int getY() {
 			return y;
 		}
-		public int getK(){
+		public void setY(int y) {
+			this.y = y;
+		}
+		public int getK() {
 			return k;
 		}
-		public boolean isRet() {
-			return blocked;
+		public void setK(int k) {
+			this.k = k;
 		}
-	}
-	public int find(int[] x, int[] y, int k) {
-		int curX = 0;
-		int curY = 0;
-
-		Set<String> points = new HashSet<String>();
+		private TheGridDivTwo getOuterType() {
+			return TheGridDivTwo.this;
+		}
 		
+	}
+	public int find(int[] x, int[] y, int k)
+	{
+		this.k = k;
+		Set<Pair> blocked = new HashSet<Pair>();
 		for(int i = 0; i < x.length; i++) {
-			points.add(x[i]+","+y[i]);
+			Pair p = new Pair(x[i], y[i], 0);
+			blocked.add(p);
 		}
 		
-		Return ret = find2(points, 0, curX, curY, k);
-		return ret.getK();
-	}
-
-	public Return find2(Set<String> points, int k, int curX, int curY, int reqK) {
-		String curPoint = curX + ","+curY;
-		if (points.contains(curPoint)) {
-			return new Return(curX, curY, k, true);
-		}
-		if(reqK == k) {
-			return new Return(curX, curY, k, true);
-		}
-
-		Return a = find2(points, k + 1, curX + 1, curY, reqK);
-		Return b = find2(points, k + 1, curX - 1, curY, reqK);
-		Return c = find2(points, k + 1, curX, curY + 1, reqK);
-		Return d = find2(points, k + 1, curX, curY - 1, reqK);
-
-		int max = max(a.getX(), b.getX(), c.getX(), d.getX());
+		Pair cur = new Pair(0, 0, 0);
+		int maxX = Integer.MIN_VALUE;
+		LinkedList<Pair> queue = new LinkedList<>();
+		queue.add(cur);
 		
-		if(max == a.getX()) {
-			return a;
+		Set<Pair> visited = new HashSet<Pair>();
+		while(!queue.isEmpty()) {
+			
+			cur = queue.removeFirst();
+			maxX = Math.max(cur.getX(), maxX);
+			
+			if(!visited.contains(cur)) {
+			
+				visited.add(cur);
+				
+				Pair east = new Pair(cur.getX() + 1, cur.getY(), cur.getK() + 1);
+				Pair west = new Pair(cur.getX() - 1, cur.getY(), cur.getK() + 1);
+				Pair north = new Pair(cur.getX(), cur.getY() + 1, cur.getK() + 1);
+				Pair south = new Pair(cur.getX(), cur.getY() - 1, cur.getK() + 1);
+				
+				addToQueue(blocked, queue, east);
+				addToQueue(blocked, queue, west);
+				addToQueue(blocked, queue, north);
+				addToQueue(blocked, queue, south);
+			}
 		}
-		else if(max == b.getX()) {
-			return b;
-		}
-		else if(max == c.getX()) {
-			return c;
-		}
-		else {
-			return d;
+		return maxX;
+	}
+	int k;
+	private void addToQueue(Set<Pair> blocked, LinkedList<Pair> queue, Pair pair) {
+		if(!blocked.contains(pair) && pair.getK() <= k) {
+			queue.add(pair);
 		}
 	}
 
-	public int max(int a, int b, int c, int d) {
-		int minab = max(a, b);
-		int mincd = max(c, d);
-
-		return max(minab, mincd);
-	}
-
-	public int max(int a, int b) {
-		return a >= b ? a : b;
-	}
-
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		long time;
 		int answer;
 		boolean errors = false;
 		int desiredAnswer;
-
+		
+		
 		time = System.currentTimeMillis();
-		answer = new TheGridDivTwo().find(new int[] { 1, 1, 1, 1 }, new int[] {
-				-2, -1, 0, 1 }, 4);
-		System.out.println("Time: " + (System.currentTimeMillis() - time)
-				/ 1000.0 + " seconds");
+		answer = new TheGridDivTwo().find(new int[]{1,1,1,1}, new int[]{-2,-1,0,1}, 4);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
 		desiredAnswer = 2;
 		System.out.println("Your answer:");
 		System.out.println("\t" + answer);
 		System.out.println("Desired answer:");
 		System.out.println("\t" + desiredAnswer);
-		if (answer != desiredAnswer) {
+		if (answer != desiredAnswer)
+		{
 			errors = true;
 			System.out.println("DOESN'T MATCH!!!!");
-		} else
+		}
+		else
 			System.out.println("Match :-)");
 		System.out.println();
 		time = System.currentTimeMillis();
-		answer = new TheGridDivTwo().find(new int[] { -1, 0, 0, 1 }, new int[] {
-				0, -1, 1, 0 }, 9);
-		System.out.println("Time: " + (System.currentTimeMillis() - time)
-				/ 1000.0 + " seconds");
+		answer = new TheGridDivTwo().find(new int[]{-1, 0, 0, 1}, new int[]{0, -1, 1, 0}, 9);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
 		desiredAnswer = 0;
 		System.out.println("Your answer:");
 		System.out.println("\t" + answer);
 		System.out.println("Desired answer:");
 		System.out.println("\t" + desiredAnswer);
-		if (answer != desiredAnswer) {
+		if (answer != desiredAnswer)
+		{
 			errors = true;
 			System.out.println("DOESN'T MATCH!!!!");
-		} else
+		}
+		else
 			System.out.println("Match :-)");
 		System.out.println();
 		time = System.currentTimeMillis();
-		answer = new TheGridDivTwo().find(new int[] {}, new int[] {}, 1000);
-		System.out.println("Time: " + (System.currentTimeMillis() - time)
-				/ 1000.0 + " seconds");
+		answer = new TheGridDivTwo().find(new int[]{}, new int[]{}, 1000);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
 		desiredAnswer = 1000;
 		System.out.println("Your answer:");
 		System.out.println("\t" + answer);
 		System.out.println("Desired answer:");
 		System.out.println("\t" + desiredAnswer);
-		if (answer != desiredAnswer) {
+		if (answer != desiredAnswer)
+		{
 			errors = true;
 			System.out.println("DOESN'T MATCH!!!!");
-		} else
+		}
+		else
 			System.out.println("Match :-)");
 		System.out.println();
 		time = System.currentTimeMillis();
-		answer = new TheGridDivTwo().find(new int[] { 1, 0, 0, -1, -1, -2, -2,
-				-3, -3, -4, -4 }, new int[] { 0, -1, 1, -2, 2, -3, 3, -4, 4,
-				-5, 5 }, 47);
-		System.out.println("Time: " + (System.currentTimeMillis() - time)
-				/ 1000.0 + " seconds");
+		answer = new TheGridDivTwo().find(new int[]{1,0,0,-1,-1,-2,-2,-3,-3,-4,-4}, new int[]{0,-1,1,-2,2,-3,3,-4,4,-5,5}, 47);
+		System.out.println("Time: " + (System.currentTimeMillis()-time)/1000.0 + " seconds");
 		desiredAnswer = 31;
 		System.out.println("Your answer:");
 		System.out.println("\t" + answer);
 		System.out.println("Desired answer:");
 		System.out.println("\t" + desiredAnswer);
-		if (answer != desiredAnswer) {
+		if (answer != desiredAnswer)
+		{
 			errors = true;
 			System.out.println("DOESN'T MATCH!!!!");
-		} else
+		}
+		else
 			System.out.println("Match :-)");
 		System.out.println();
-
+		
+		
 		if (errors)
 			System.out.println("Some of the test cases had errors :-(");
 		else
-			System.out
-					.println("You're a stud (at least on the test data)! :-D ");
+			System.out.println("You're a stud (at least on the test data)! :-D ");
 	}
 
 }
-// Powered by [KawigiEdit] 2.0!
+//Powered by [KawigiEdit] 2.0!
